600cdd570702869b7674371959e1cbf85df1d225
Corey Lowman <corey.lowman@jhuapl.edu>
2021-11-17 17:17:25 -0500

    Updating example agents with new api. Changing print agent to logging calls and indenting

---
+++ examples/logging_agent.py
@@ -0,0 +1,70 @@
+import typing
+import gym
+from tella.agent import Agent, Observation, Action
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class LoggingAgent(Agent):
+    def block_start(self, is_learning_allowed: bool) -> None:
+        if is_learning_allowed:
+            logger.info("About to start a new learning block")
+        else:
+            logger.info("About to start a new evaluation block")
+
+    def task_start(
+        self,
+        observation_space: gym.Space,
+        action_space: gym.Space,
+        task_name: typing.Optional[str],
+        variant_name: typing.Optional[str],
+    ) -> None:
+        logger.info(
+            f"\tAbout to start interacting with a new task. {observation_space=} {action_space=} {task_name=} {variant_name=}"
+        )
+
+    def episode_start(self) -> None:
+        logger.info("\t\tAbout to start a new episode")
+
+    def step(self, observation: Observation) -> Action:
+        logger.info("\t\t\tReturn random action")
+        return self.action_space.sample()
+
+    def step_result(
+        self,
+        observation: Observation,
+        action: Action,
+        reward: float,
+        done: bool,
+        next_observation: Observation,
+    ) -> None:
+        logger.info(f"\t\t\tReceived step result {done=}")
+
+    def episode_end(self) -> None:
+        logger.info("\t\tEpisode just ended")
+
+    def task_end(
+        self,
+        observation_space: gym.Space,
+        action_space: gym.Space,
+        task_name: typing.Optional[str],
+        variant_name: typing.Optional[str],
+    ) -> None:
+        logger.info(
+            f"\tDone interacting with task. {observation_space=} {action_space=} {task_name=} {variant_name=}"
+        )
+
+    def block_end(self, is_learning_allowed: bool) -> None:
+        if is_learning_allowed:
+            logger.info("Done with learning block")
+        else:
+            logger.info("Done with evaluation block")
+
+    def save_internal_state(self, path: str) -> bool:
+        logger.info("Saving internal state")
+        return False
+
+    def restore_internal_state(self, path: str) -> bool:
+        logger.info("Restoring internal state")
+        return False
--- examples/minimal_agent.py
+++ examples/minimal_agent.py
@@ -2,5 +2,11 @@ from tella.agent import Agent, Observation, Action
 
 
 class MinimalRandomAgent(Agent):
-    def get_action(self, observation: Observation, **kwargs) -> Action:
+    def step(self, observation: Observation) -> Action:
         return self.action_space.sample()
+
+    def save_internal_state(self, path: str) -> bool:
+        return False
+
+    def restore_internal_state(self, path: str) -> bool:
+        return False

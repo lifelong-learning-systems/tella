9db700897bde1019dab73a917200100d41157bc5
Corey Lowman <corey.lowman@jhuapl.edu>
2021-11-29 14:03:31 -0500

    Moving experience from curriculum to subpackage, and moving rl_experience to new subpackage

--- tella/curriculum.py
+++ tella/curriculum.py
@@ -1,12 +1,9 @@
 import typing
 import abc
-
-InputType = typing.TypeVar("InputType")
-OutputType = typing.TypeVar("OutputType")
-InfoType = typing.TypeVar("InfoType")
+from .experiences.experience import GenericExperience
 
 
-class Curriculum(abc.ABC, typing.Generic[InputType, OutputType, InfoType]):
+class Curriculum(abc.ABC, typing.Generic[GenericExperience]):
     """
     Represents a lifelong/continual learning curriculum. A curriculum is simply
     a sequence of :class:`Block`s.
@@ -15,13 +12,13 @@ class Curriculum(abc.ABC, typing.Generic[InputType, OutputType, InfoType]):
     @abc.abstractmethod
     def blocks(
         self,
-    ) -> typing.Iterable["Block[InputType, OutputType, InfoType]"]:
+    ) -> typing.Iterable["Block[GenericExperience]"]:
         """
         :return: An Iterable of :class:`Block`.
         """
 
 
-class Block(abc.ABC, typing.Generic[InputType, OutputType, InfoType]):
+class Block(abc.ABC, typing.Generic[GenericExperience]):
     """
     This represents a sequence of :class:`Experience`.
 
@@ -44,96 +41,45 @@ class Block(abc.ABC, typing.Generic[InputType, OutputType, InfoType]):
         """
 
     @abc.abstractmethod
-    def experiences(
-        self,
-    ) -> typing.Iterable["Experience[InputType, OutputType, InfoType]"]:
+    def experiences(self) -> typing.Iterable[GenericExperience]:
         """
         :return: An Iterable of :class:`Experience`.
         """
 
 
-class Experience(abc.ABC, typing.Generic[InputType, OutputType, InfoType]):
-    """
-    An experience transforms some object of type `InputType` into an object
-    with type `OutputType`. Additionally, an experience has some InfoTypermation
-    associated with it, of type `InfoType`.
-
-    For RL, an experience can be thought of as taking an agent as InputType,
-    producing an Iterable of MDP transitions as OutputType, and giving
-    and :class:`gym.Env` as the InfoType.
-
-    For image classification, an experience could be a batch size integer as
-    InputType, Batch of image/label data as OutputType, and giving
-    the dataset object as InputType.
-    """
-
-    @abc.abstractmethod
-    def validate(self) -> None:
-        """
-        A method to validate that the experience is set up properly.
-
-        This should raise an Exception if the experience is not set up properly.
-        """
-
-    @abc.abstractmethod
-    def info(self) -> InfoType:
-        """
-        :return: The object of type `InfoType` associated with this experience.
-        """
-
-    @abc.abstractmethod
-    def generate(self, inp: InputType) -> OutputType:
-        """
-        The main method to generate the Experience data.
-
-        :param inp: The object of type InputType.
-        :return: The data for the experience.
-        """
-
-
-class LearnBlock(Block[InputType, OutputType, InfoType]):
+class LearnBlock(Block[GenericExperience]):
     """
     A helper class to create learning blocks. Experiences are passed into the
     constructor, and is_learning_allowed always returns True.
     """
 
-    def __init__(
-        self,
-        experiences: typing.Iterable[Experience[InputType, OutputType, InfoType]],
-    ) -> None:
+    def __init__(self, experiences: typing.Iterable[GenericExperience]) -> None:
         self._experiences = experiences
 
-    def experiences(
-        self,
-    ) -> typing.Iterable[Experience[InputType, OutputType, InfoType]]:
+    def experiences(self) -> typing.Iterable[GenericExperience]:
         return self._experiences
 
     def is_learning_allowed(self) -> bool:
         return True
 
 
-class EvalBlock(Block[InputType, OutputType, InfoType]):
+class EvalBlock(Block[GenericExperience]):
     """
     A helper class to create evaluation blocks. Experiences are passed into the
     constructor, and is_learning_allowed always returns False.
     """
 
-    def __init__(
-        self,
-        experiences: typing.Iterable[Experience[InputType, OutputType, InfoType]],
-    ) -> None:
+    def __init__(self, experiences: typing.Iterable[GenericExperience]) -> None:
         self._experiences = experiences
 
-    def experiences(
-        self,
-    ) -> typing.Iterable[Experience[InputType, OutputType, InfoType]]:
+    def experiences(self) -> typing.Iterable[GenericExperience]:
         return self._experiences
 
     def is_learning_allowed(self) -> bool:
         return False
 
 
-class InterleavedEvalCurriculum(Curriculum[InputType, OutputType, InfoType]):
+class InterleavedEvalCurriculum(Curriculum[GenericExperience]):
     """
     One possible version of a curriculum where a single evaluation block
     is interleaved between a sequence of learning blocks.
@@ -148,26 +94,20 @@ class InterleavedEvalCurriculum(Curriculum[InputType, OutputType, InfoType]):
     """
 
     @abc.abstractmethod
-    def learn_blocks(
-        self,
-    ) -> typing.Iterable[LearnBlock[InputType, OutputType, InfoType]]:
+    def learn_blocks(self) -> typing.Iterable[LearnBlock[GenericExperience]]:
         """
         :return: An iterable of :class:`LearnBlock`.
         """
 
     @abc.abstractmethod
-    def eval_block(
-        self,
-    ) -> EvalBlock[InputType, OutputType, InfoType]:
+    def eval_block(self) -> EvalBlock[GenericExperience]:
         """
         :return: The single :class:`EvalBlock` to interleave between each
             individual :class:`LearnBlock` returned from
-                :meth:`InterleavedEvalCurriculum.learn_blocks`.
+            :meth:`InterleavedEvalCurriculum.learn_blocks`.
         """
 
-    def blocks(
-        self,
-    ) -> typing.Iterable[Block[InputType, OutputType, InfoType]]:
+    def blocks(self) -> typing.Iterable[Block[GenericExperience]]:
         yield self.eval_block()
         for block in self.learn_blocks():
             yield block
---
+++ tella/experiences/rl.py
@@ -0,0 +1,93 @@
+import typing
+import gym
+from .experience import Experience
+from ..validation import validate_params
+
+Observation = typing.TypeVar("Observation")
+Action = typing.TypeVar("Action")
+Reward = float
+Done = bool
+
+MDPTransition = typing.Tuple[Observation, Action, Reward, Done, Observation]
+"""
+A tuple with data representing a transition of an MDP. The last item of the tuple
+is the resulting observation that happens after applying action to the first
+observation in the tuple.
+"""
+
+ActionFn = typing.Callable[[typing.List[Observation]], typing.List[Action]]
+"""
+A function that takes a list of Observations and returns a list of Actions, one
+for each observation.
+"""
+
+RLExperience = Experience[ActionFn, typing.Iterable[MDPTransition], gym.Env]
+"""
+An RLExperience is an Experience that takes an ActionFn as input, produces an
+Iterable[MDPTransition], and returns a :class:`gym.Env` as the Information.
+"""
+
+
+class LimitedEpisodesExperience(RLExperience):
+    """
+    Represents an experience that consists of a set number of episodes in a :class:`gym.Env`.
+
+    This is a subclass of the :class:`RLExperience`, which is defined as an :class:`Experience`
+    that takes an :type:`ActionFn` and returns an iterable of :type:`Transition`.
+    """
+
+    def __init__(
+        self,
+        task_cls: typing.Type[gym.Env],
+        *,
+        num_episodes: int,
+        num_envs: typing.Optional[int] = None,
+        params: typing.Optional[typing.Dict] = None,
+    ) -> None:
+        if num_envs is None:
+            num_envs = 1
+        if params is None:
+            params = {}
+        assert num_envs > 0
+
+        self._task_cls = task_cls
+        self._params = params
+        self._num_episodes = num_episodes
+        self._num_envs = num_envs
+        self._env = None
+
+    def validate(self) -> None:
+        return validate_params(self._task_cls, list(self._params.keys()))
+
+    def _make_env(self) -> gym.Env:
+        return self._task_cls(**self._params)
+
+    def info(self) -> gym.Env:
+        if self._env is None:
+            vector_env_cls = gym.vector.AsyncVectorEnv
+            if self._num_envs == 1:
+                vector_env_cls = gym.vector.SyncVectorEnv
+            self._env = vector_env_cls([self._make_env for _ in range(self._num_envs)])
+        return self._env
+
+    def generate(self, action_fn: ActionFn) -> typing.Iterable[MDPTransition]:
+        env = self.info()
+        num_episodes_left = self._num_episodes
+        observations = env.reset()
+        while num_episodes_left > 0:
+            # FIXME: this always uses all environments in the vector env. this will leak extra episodes to the consumer
+            actions = action_fn(observations)
+            next_observations, rewards, dones, infos = env.step(actions)
+            for i in range(self._num_envs):
+                # FIXME: if done[i] == True, then we need to return info[i]["terminal_observation"]
+                yield (
+                    observations[i],
+                    actions[i],
+                    rewards[i],
+                    dones[i],
+                    next_observations[i],
+                )
+            observations = next_observations
+            num_episodes_left -= sum(dones)
+        self._env.close()
+        self._env = None

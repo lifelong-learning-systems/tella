2f08759183afbd8226370228944c8f87c3645932
Corey Lowman <corey.lowman@jhuapl.edu>
2021-12-09 12:42:20 -0500

    #38 Changing cli tests to use unittest.mock

--- tests/test_rl_cli.py
+++ tests/test_rl_cli.py
@@ -1,18 +1,32 @@
+import argparse
+from unittest.mock import patch
 from tella.rl_cli import rl_cli
 from .simple_curriculum import SimpleRLCurriculum
 from .simple_agent import SimpleRLAgent
 
 
-def test_no_args():
+@patch(
+    "argparse.ArgumentParser.parse_args",
+    return_value=argparse.Namespace(num_envs=1, num_lifetimes=1, log_dir=""),
+)
+def test_no_args(p):
     # TODO what should this test other than being runnable?
-    rl_cli(SimpleRLAgent, SimpleRLCurriculum, parse_args=[])
+    rl_cli(SimpleRLAgent, SimpleRLCurriculum)
 
 
-def test_num_runs():
+@patch(
+    "argparse.ArgumentParser.parse_args",
+    return_value=argparse.Namespace(num_envs=1, num_lifetimes=2, log_dir=""),
+)
+def test_num_lifetimes(p):
     # TODO what should this test other than being runnable?
-    rl_cli(SimpleRLAgent, SimpleRLCurriculum, parse_args=["--num-runs", "2"])
+    rl_cli(SimpleRLAgent, SimpleRLCurriculum)
 
 
-def test_num_cores():
+@patch(
+    "argparse.ArgumentParser.parse_args",
+    return_value=argparse.Namespace(num_envs=2, num_lifetimes=1, log_dir=""),
+)
+def test_num_envs(p):
     # TODO what should this test other than being runnable?
-    rl_cli(SimpleRLAgent, SimpleRLCurriculum, parse_args=["--num-cores", "2"])
+    rl_cli(SimpleRLAgent, SimpleRLCurriculum)

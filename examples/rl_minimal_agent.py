c2707b8021984ad4c4ac43aff7f9142a428b7868
Gino Perrotta <Gino.Perrotta@jhuapl.edu>
2022-01-03 10:22:09 -0500

    Remove optional method from rl_minimal_agent

--- examples/rl_minimal_agent.py
+++ examples/rl_minimal_agent.py
@@ -18,9 +18,6 @@ class MinimalRandomAgent(tella.ContinualRLAgent):
     def receive_transition(self, transition: tella.Transition):
         pass
 
-    def set_rng_seed(self, seed: int) -> None:
-        self.action_space.seed(seed)
-
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)

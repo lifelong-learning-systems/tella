75404a2a8ed6afcaebf0661dfd2a4a758757a7f0
Cash Costello <cash.costello@jhuapl.edu>
2022-01-07 14:23:57 -0500

    don't need to import envs

--- tella/_curriculums/minigrid/__init__.py
+++ tella/_curriculums/minigrid/__init__.py
@@ -19,7 +19,5 @@ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
 IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 
-# Import the envs module so that envs register themselves
-import envs
-from simple import SimpleMiniGridCurriculum
-from m21 import MiniGridCondensed, MiniGridDispersed
+from .simple import SimpleMiniGridCurriculum
+from .m21 import MiniGridCondensed, MiniGridDispersed

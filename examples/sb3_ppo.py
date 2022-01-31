fae6f7d8b0f936da8c8d81c5a168cf8a37cf7170
Corey Lowman <corey.lowman@jhuapl.edu>
2022-01-31 16:32:44 -0500

    Fixing reproducibility issues with sb3

--- examples/sb3_ppo.py
+++ examples/sb3_ppo.py
@@ -10,13 +10,25 @@ import torch
 
 logger = logging.getLogger("SB3 PPO")
 
+BASE_HYPERPARAMETERS = {
+    "policy": "MlpPolicy",
+    "n_steps": 128,
+    "batch_size": 64,
+    "gae_lambda": 0.95,
+    "gamma": 0.99,
+    "n_epochs": 4,
+    "ent_coef": 0.0,
+    "learning_rate": 2.5e-4,
+    "clip_range": 0.2,
+}
+
 
 class _DummyEnv(gym.Env):
     def __init__(self, observation_space, action_space) -> None:
         super().__init__()
         self.observation_space = observation_space
         self.action_space = action_space
-        self.observations = [self.observation_space.sample()]
+        self.observations = [np.zeros(self.observation_space.shape)]
 
     def reset(self):
         # NOTE: should only be called once during PPO._setup_learn()
@@ -49,21 +61,33 @@ class SB3PPOAgent(tella.ContinualRLAgent):
         )
         self.obs_is_image = is_image_space(observation_space)
 
-        # TODO what are the hyperparameters here?
-        self.ppo = PPO("MlpPolicy", _DummyEnv(observation_space, action_space))
+        logging.info(f"Seeding {rng_seed}")
+        # NOTE: without wrapping rng_seed get this error "ValueError: Seed must be between 0 and 2**32 - 1"
+        torch.use_deterministic_algorithms(True)
+        self.ppo = PPO(
+            env=_DummyEnv(observation_space, action_space),
+            seed=rng_seed % (2**32),
+            verbose=1,
+            device="cpu",
+            **BASE_HYPERPARAMETERS,
+        )
         self.ppo._setup_learn(np.inf, eval_env=None)
 
         self.steps_since_last_train = 0
         self.last_dones = [False] * self.num_envs
+        self.iteration = 0
+        self.num_timesteps = 0
 
     def task_variant_start(
         self, task_name: typing.Optional[str], variant_name: typing.Optional[str]
     ) -> None:
-        self.last_dones = [False] * self.num_envs
+        logger.info(f"Starting rollout on {task_name} {variant_name}")
+        self.last_dones = [True] * self.num_envs
         self.ppo.rollout_buffer.reset()
         self.steps_since_last_train = 0
         if self.ppo.use_sde:
             self.ppo.policy.reset_noise(self.num_envs)
+        self.ppo.policy.set_training_mode(False)
 
     def choose_actions(
         self, observations: typing.List[typing.Optional[tella.Observation]]
@@ -85,7 +109,9 @@ class SB3PPOAgent(tella.ContinualRLAgent):
             # Convert to pytorch tensor or to TensorDict
             obs_tensor = obs_as_tensor(self._last_obs, self.ppo.device)
             actions, self.last_values, self.last_log_probs = self.ppo.policy.forward(
-                obs_tensor
+                obs_tensor,
+                deterministic=not self.is_learning_allowed
+                # TODO what should deterministic be?
             )
         actions = actions.cpu().numpy()
 
@@ -109,6 +135,7 @@ class SB3PPOAgent(tella.ContinualRLAgent):
         assert all(t is not None for t in transitions)
 
         self.steps_since_last_train += 1
+        self.num_timesteps += len(transitions)
 
         obss, actions, rewards, dones, next_obss = list(
             map(np.array, zip(*transitions))
@@ -122,13 +149,16 @@ class SB3PPOAgent(tella.ContinualRLAgent):
             # Reshape in case of discrete action
             actions = actions.reshape(-1, 1)
 
+        next_obss_t = obs_as_tensor(next_obss, self.ppo.device)
+        with torch.no_grad():
+            # Compute value for the last timestep
+            next_obss_values = self.ppo.policy.predict_values(next_obss_t)
+
         for idx, done in enumerate(dones):
-            if done:
-                # FIXME: this doesn't account for time limits like original SB3 code
-                terminal_obs = self.ppo.policy.obs_to_tensor(next_obss[idx])[0]
-                with torch.no_grad():
-                    terminal_value = self.ppo.policy.predict_values(terminal_obs)[0]
-                rewards[idx] += self.ppo.gamma * terminal_value
+            # FIXME: how do we get access to truncated data from TimeLimit
+            truncated = False
+            if done and truncated:
+                rewards[idx] += self.ppo.gamma * next_obss_values[idx].item()
 
         self.ppo.rollout_buffer.add(
             obss,
@@ -141,18 +171,19 @@ class SB3PPOAgent(tella.ContinualRLAgent):
         self.last_dones = dones
 
         if self.steps_since_last_train >= self.ppo.n_steps:
-            with torch.no_grad():
-                # Compute value for the last timestep
-                values = self.ppo.policy.predict_values(
-                    obs_as_tensor(next_obss, self.ppo.device)
-                )
-
             self.ppo.rollout_buffer.compute_returns_and_advantage(
-                last_values=values, dones=dones
+                last_values=next_obss_values, dones=dones
             )
+
+            self.iteration += 1
+
+            self.ppo.logger.record("time/iterations", self.iteration)
+            self.ppo.logger.record("time/total_timesteps", self.num_timesteps)
+            self.ppo.logger.dump(step=self.num_timesteps)
 
             self.ppo.train()
 
+            self.ppo.policy.set_training_mode(False)
             self.steps_since_last_train = 0
             self.ppo.rollout_buffer.reset()
 

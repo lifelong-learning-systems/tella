e777b1c1f5cd5592e5f366768462e4b472420dd9
Gino Perrotta <Gino.Perrotta@jhuapl.edu>
2022-02-02 16:53:44 -0500

    Change test to match github workflow working directory

--- tests/test_m21_curriculum.py
+++ tests/test_m21_curriculum.py
@@ -240,7 +240,13 @@ def test_configured_block_limits_value_error():
         curriculum._block_limit_from_config("SimpleCrossing", "")
 
 
-@pytest.mark.parametrize("config_file", glob.glob("../examples/configs/*.yml"))
+def test_find_example_configs():
+    example_configs = glob.glob("examples/configs/*.yml")
+    expected_number = 3  # Update if configs added or removed
+    assert len(example_configs) == expected_number
+
+
+@pytest.mark.parametrize("config_file", glob.glob("examples/configs/*.yml"))
 def test_example_configurations(config_file: str):
     curriculum = MiniGridDispersed(rng_seed=0, config_file=config_file)
     validate_curriculum(curriculum)

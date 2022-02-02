c1b454a847e3876c59b0e17e2506b568fb6d7471
Gino Perrotta <Gino.Perrotta@jhuapl.edu>
2022-02-02 11:00:11 -0500

    Group mocked configs and add tests for errors

--- tests/test_m21_curriculum.py
+++ tests/test_m21_curriculum.py
@@ -38,6 +38,42 @@ class TestMiniGridReducedActionSpaceWrapper:
             wrapper.action(3)
 
 
+CONFIG_STEP_LIMIT = """
+# This is a mocked YAML file to be loaded as a test config
+---
+learn:
+    default unit: steps
+"""
+
+
+CONFIG_PER_TASK = """
+# This is a mocked YAML file to be loaded as a test config
+---
+learn:
+    default length: 999
+    CustomFetchS16T2N4: 1234
+    SimpleCrossing: 42
+num learn blocks: 5
+"""
+
+
+CONFIG_FORMAT_ERROR = """
+# This is a mocked YAML file to be loaded as a test config
+---
+learn:
+    default length:
+        - This is not the expected format
+"""
+
+
+CONFIG_VALUE_ERROR = """
+# This is a mocked YAML file to be loaded as a test config
+---
+learn:
+    SimpleCrossing: This is not an integer
+"""
+
+
 def test_curriculum_default_configuration():
     curriculum = MiniGridDispersed(rng_seed=0)
     task_info = [
@@ -69,14 +105,7 @@ def test_curriculum_default_configuration():
 
 @mock.patch(
     "builtins.open",
-    mock.mock_open(
-        read_data=(
-            "# This is a fake yaml file to be loaded as a test config\n"
-            "---\n"
-            "learn:\n"
-            "    default unit: steps\n"
-        )
-    ),
+    mock.mock_open(read_data=CONFIG_STEP_LIMIT),
 )
 def test_curriculum_file_configuration_step_limit():
     curriculum = MiniGridDispersed(rng_seed=0, config_file="mocked.yml")
@@ -117,17 +146,7 @@ def test_curriculum_file_configuration_step_limit():
 
 @mock.patch(
     "builtins.open",
-    mock.mock_open(
-        read_data=(
-            "# This is a fake yaml file to be loaded as a test config\n"
-            "---\n"
-            "learn:\n"
-            "    default length: 999\n"
-            "    CustomFetchS16T2N4: 1234\n"
-            "    SimpleCrossing: 42\n"
-            "num learn blocks: 5\n"
-        )
-    ),
+    mock.mock_open(read_data=CONFIG_PER_TASK),
 )
 def test_curriculum_file_configuration_per_task():
     curriculum = MiniGridDispersed(
@@ -172,14 +191,7 @@ def test_default_block_limits():
 
 @mock.patch(
     "builtins.open",
-    mock.mock_open(
-        read_data=(
-            "# This is a fake yaml file to be loaded as a test config\n"
-            "---\n"
-            "learn:\n"
-            "    default unit: steps\n"
-        )
-    ),
+    mock.mock_open(read_data=CONFIG_STEP_LIMIT),
 )
 def test_configured_block_limits_step_limit():
     curriculum = MiniGridDispersed(rng_seed=0, config_file="mocked.yml")
@@ -189,19 +201,7 @@ def test_configured_block_limits_step_limit():
 
 @mock.patch(
     "builtins.open",
-    mock.mock_open(
-        read_data=(
-            "# This is a fake yaml file to be loaded as a test config\n"
-            "---\n"
-            "learn:\n"
-            "    default length: 999\n"
-            "    CustomFetchS16T2N4: 1234\n"
-            "    SimpleCrossing:\n"
-            "        length: 42\n"
-            "        unit: steps\n"
-            "num learn blocks: 5\n"
-        )
-    ),
+    mock.mock_open(read_data=CONFIG_PER_TASK),
 )
 def test_configured_block_limits_per_task():
     curriculum = MiniGridDispersed(rng_seed=0, config_file="mocked.yml")
@@ -214,5 +214,25 @@ def test_configured_block_limits_per_task():
     }
 
     assert curriculum._block_limit_from_config("SimpleCrossing", "") == {
-        "num_steps": 42
+        "num_episodes": 42
     }
+
+
+@mock.patch(
+    "builtins.open",
+    mock.mock_open(read_data=CONFIG_FORMAT_ERROR),
+)
+def test_configured_block_limits_format_error():
+    curriculum = MiniGridDispersed(rng_seed=0, config_file="mocked.yml")
+    with pytest.raises(AssertionError):
+        curriculum._block_limit_from_config("", "")
+
+
+@mock.patch(
+    "builtins.open",
+    mock.mock_open(read_data=CONFIG_VALUE_ERROR),
+)
+def test_configured_block_limits_value_error():
+    curriculum = MiniGridDispersed(rng_seed=0, config_file="mocked.yml")
+    with pytest.raises(ValueError):
+        curriculum._block_limit_from_config("SimpleCrossing", "")

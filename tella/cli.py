0203e8bca46b971eaddf76c714d2db1a9d926a15
Gino Perrotta <Gino.Perrotta@jhuapl.edu>
2022-03-10 09:11:12 -0500

    Remove requirement for curriculum seed CLI arg

--- tella/cli.py
+++ tella/cli.py
@@ -133,7 +133,6 @@ def _build_parser(require_curriculum: bool) -> argparse.ArgumentParser:
         "--curriculum-seed",
         default=None,
         type=int,
-        required=True,
         help="The curriculum rng seed to use for reproducibility.",
     )
     parser.add_argument(

5f14a657724985dc578a0b875609ea07bc2c7ab0
Gino Perrotta <Gino.Perrotta@jhuapl.edu>
2022-05-03 14:56:22 -0400

    Add agent name to "scenario_dir" so that l2logger uses it in filepath.

--- tella/experiment.py
+++ tella/experiment.py
@@ -230,7 +230,10 @@ def run(
     :param log_dir: Directory for l2logger files.
     :param num_envs: Number of parallel environments.
     """
-    scenario_dir = curriculum.__class__.__name__
+    scenario_dir = (
+        f"{curriculum.__class__.__name__}-"
+        f"{agent.__class__.__name__}"
+    )
     scenario_info = {
         "author": "JHU APL",
         "complexity": "1-low",
